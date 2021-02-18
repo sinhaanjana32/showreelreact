d854a3c7673966262c268f21c47f8afe86d930ce
Anjana <sinhaanjana32@gmail.com>
2021-02-18 12:28:28 +0200

    add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Show from "./Show";
+
+function App() {
+  return (
+    <div className="App">
+      <Show />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Show.js
@@ -0,0 +1,406 @@
+import React, { useEffect, useRef } from "react";
+import * as d3 from "d3";
+
+const Show = () => {
+  useEffect(() => {
+    var svg = d3.select("svg"),
+      svgNode = svg.node().getBoundingClientRect(),
+      width = svgNode.width,
+      height = svgNode.height,
+      duration = 2000,
+      delay = 500,
+      colorCategoryScale = d3
+        .scaleOrdinal()
+        .range([
+          "red",
+          "green",
+          "blue",
+          "#6b486b",
+          "#a05d56",
+          "#d0743c",
+          "#ff8c00",
+        ]),
+      colorDepthScale,
+      colorSchemes = [
+        "Inferno",
+        "Magma",
+        "Plasma",
+        "Warm",
+        "Cool",
+        "Blues",
+        "BrBG",
+        "BuGn",
+        "BuPu",
+        "GnBu",
+        "Greens",
+        "Greys",
+        "Oranges",
+        "OrRd",
+        "PiYG",
+        "PRGn",
+        "PuBu",
+        "PuBuGn",
+        "PuOr",
+        "PuRd",
+        "Purples",
+        "RdBu",
+        "RdGy",
+        "RdPu",
+        "RdYlBu",
+        "RdYlGn",
+        "Reds",
+        "Spectral",
+        "YlGn",
+        "YlGnBu",
+        "YlOrBr",
+        "YlOrRd",
+      ];
+
+    svg = svg.append("g");
+
+    d3.json("flare.json").then((data) => {
+      console.log(data);
+      showReel(data);
+    });
+
+    async function showReel(flare) {
+      let colorScheme =
+        colorSchemes[Math.floor(Math.random() * colorSchemes.length)];
+      colorDepthScale = d3
+        .scaleSequential(d3[`interpolate${colorScheme}`])
+        .domain([0, 4]);
+      await treeMap(flare);
+      await changeRectToCirc();
+      await bubbleChart(flare);
+      await circlePack(flare);
+      await shrink();
+      await radialTree(flare);
+      await endShowreel();
+      showReel(flare);
+    }
+
+    function treeMap(flare) {
+      return new Promise((resolve) => {
+        var treemap = d3
+          .treemap()
+          .tile(d3.treemapResquarify)
+          .size([width, height])
+          .round(true)
+          .paddingInner(1);
+
+        var root = d3
+          .hierarchy(flare)
+          .eachBefore(
+            (d) =>
+              (d.data.id =
+                (d.parent ? d.parent.data.name + "." : "") + d.data.name)
+          )
+          .sum((d) => d.size);
+
+        treemap(root);
+
+        var cell = svg
+          .selectAll("g")
+          .data(root.leaves())
+          .enter()
+          .append("g")
+          .attr("class", "node")
+          .attr("transform", (d) => "translate(" + d.x0 + "," + d.y0 + ")")
+          .style("opacity", 0)
+          .sort((a, b) => a.x0 - b.x0 + (b.y0 - a.y0));
+
+        cell
+          .append("rect")
+          .attr("id", (d) => d.data.name)
+          .attr("width", (d) => d.x1 - d.x0)
+          .attr("height", (d) => d.y1 - d.y0)
+          .attr("fill", (d) => colorCategoryScale(d.parent.data.name));
+
+        cell
+          .transition()
+          .duration((d, i) => Math.random() * (1000 - 500) + 500)
+          .delay((d, i) => Math.random() * (1000 - 100) + 100)
+          .style("opacity", 1)
+          .call(endAll, sumByCount);
+
+        function sumByCount() {
+          treemap(root.sum((d) => (d.children ? 0 : 1)));
+          cell
+            .transition()
+            .duration(duration)
+            .attr("transform", (d) => "translate(" + d.x0 + "," + d.y0 + ")")
+            .select("rect")
+            .attr("width", (d) => d.x1 - d.x0)
+            .attr("height", (d) => d.y1 - d.y0)
+            .call(endAll, resolve);
+        }
+      });
+    }
+
+    function changeRectToCirc() {
+      return new Promise((resolve) => {
+        var node = svg.selectAll(".node");
+        node
+          .select("rect")
+          .attr("rx", "0")
+          .attr("ry", "0")
+          .transition()
+          .duration(duration * 0.5)
+          .ease(d3.easeCubicIn)
+          .attr("x", (d) => (d.x1 - d.x0) / 2)
+          .attr("y", (d) => (d.y1 - d.y0) / 2)
+          .attr("width", "10px")
+          .attr("height", "10px")
+          .remove();
+
+        setTimeout(() => {
+          var radius = 5;
+          node
+            .append("circle")
+            .attr("r", radius)
+            .attr("cx", (d) => (d.x1 - d.x0) / 2 + radius)
+            .attr("cy", (d) => (d.y1 - d.y0) / 2 + radius)
+            .attr("fill", (d) => colorCategoryScale(d.parent.data.name));
+
+          resolve();
+        }, duration * 0.35);
+      });
+    }
+
+    function bubbleChart(flare) {
+      return new Promise((resolve) => {
+        var pack = d3.pack().size([width, height]).padding(1.5);
+
+        var flatData = [];
+        d3.hierarchy(flare).each((d) => {
+          if (!d.children)
+            flatData.push({
+              name: d.data.name,
+              parent: d.parent.data.name,
+              size: d.data.size,
+            });
+        });
+
+        var root = d3
+          .hierarchy({
+            children: flatData,
+          })
+          .sum((d) => d.size);
+
+        var node = svg
+          .selectAll(".node")
+          .data(pack(root).leaves(), (d) => d.data.name);
+
+        node
+          .transition("grow")
+          .select("circle")
+          .transition()
+          .duration(duration * 0.5)
+          .ease(d3.easeCubicOut)
+          .attr("r", (d) => d.r);
+
+        setTimeout(() => {
+          node
+            .transition("move")
+            .duration(duration)
+            .attr("transform", (d) => "translate(" + d.x + "," + d.y + ")");
+          node
+            .select("circle")
+            .transition()
+            .duration(duration)
+            .attr("cx", 0)
+            .attr("cy", 0)
+            .call(endAll, () => setTimeout(resolve, delay));
+        }, duration * 0.35);
+      });
+    }
+
+    //leaf nodes gather together, then reveal parents
+    function circlePack(flare) {
+      return new Promise((resolve) => {
+        var pack = d3.pack().size([width, height]).padding(2.5);
+        var root = d3
+          .hierarchy(flare)
+          .sum((d) => d.size)
+          .sort((a, b) => b.value - a.value);
+
+        var node = svg
+          .selectAll(".node")
+          .data(pack(root).descendants(), (d) => d.data.name);
+
+        //entering nodes are only parents
+        let nodeEnter = node
+          .enter()
+          .append("g")
+          .attr("class", "node")
+          .attr("transform", (d) => "translate(" + d.x + "," + d.y + ")");
+        nodeEnter
+          .append("circle")
+          .attr("r", (d) => d.r)
+          .style("fill", (d) => colorDepthScale(d.depth))
+          .style("opacity", 0);
+
+        node = node
+          .merge(nodeEnter)
+          .order()
+          .attr("class", (d) => {
+            return (
+              "node" +
+              (!d.parent
+                ? " node--root"
+                : d.children
+                ? " node--internal"
+                : " node--leaf")
+            );
+          });
+
+        node
+          .transition()
+          .duration(duration)
+          .attr("transform", (d) => "translate(" + d.x + "," + d.y + ")")
+          .select("circle")
+          .attr("r", (d) => d.r)
+          .style("fill", (d) => colorDepthScale(d.depth))
+          .style("opacity", 1)
+          .call(endAll, () => setTimeout(resolve, delay));
+      });
+    }
+
+    function shrink() {
+      return new Promise((resolve) => {
+        svg
+          .select(".node--root circle")
+          .style("stroke", "#333")
+          .style("stroke-width", 0.5)
+          .style("stroke-opacity", 0)
+          .transition("opacity")
+          .duration(duration)
+          .style("stroke-opacity", 0.5);
+
+        svg
+          .selectAll(".node circle")
+          .transition()
+          .duration(duration / 2)
+          .attr("r", 2.5)
+          .on("end", resolve);
+      });
+    }
+
+    function radialTree(flare) {
+      return new Promise((resolve) => {
+        var root = d3
+          .hierarchy(flare)
+          .sum((d) => d.size)
+          .sort((a, b) => b.value - a.value);
+
+        var tree = d3
+          .tree()
+          .size([2 * Math.PI, 325])
+          .separation((a, b) => (a.parent == b.parent ? 1 : 2) / a.depth);
+
+        tree(root);
+
+        svg
+          .transition()
+          .duration(duration)
+          .attr("transform", `translate(${width / 2} ${height / 2})`);
+
+        var node = svg
+          .selectAll(".node")
+          .data(root.descendants(), (d) => d.data.name);
+
+        node
+          .transition()
+          .duration(duration)
+          .attr("transform", (d) => "translate(" + radialPoint(d.x, d.y) + ")")
+          .select("circle")
+          .attr("r", 2.5)
+          .call(endAll, showLinks);
+
+        function showLinks() {
+          var pathLengths = [];
+          let pathDuration = duration / root.descendants()[0].height;
+
+          svg
+            .selectAll(".link")
+            .data(root.links())
+            .enter()
+            .append("path")
+            .attr("class", "link")
+            //.style('opacity',0)
+            .attr(
+              "d",
+              d3
+                .linkRadial()
+                .angle((d) => d.x)
+                .radius((d) => d.y)
+            )
+            .each(function () {
+              var length = d3.select(this).node().getTotalLength();
+              pathLengths.push(length);
+            })
+            .attr(
+              "stroke-dasharray",
+              (d, i) => pathLengths[i] + " " + pathLengths[i]
+            )
+            .attr("stroke-dashoffset", (d, i) => pathLengths[i])
+            .transition()
+            .duration(pathDuration)
+            .delay((d) => d.source.depth * pathDuration)
+            .ease(d3.easeLinear)
+            .attr("stroke-dashoffset", 0)
+            .call(endAll, () => setTimeout(resolve, delay));
+        }
+      });
+
+      function radialPoint(x, y) {
+        return [(y = +y) * Math.cos((x -= Math.PI / 2)), y * Math.sin(x)];
+      }
+    }
+    //transitioning to scale(0) breaks rotate
+    function endShowreel() {
+      return new Promise((resolve) => {
+        svg
+          .attr("transform", `translate(${width / 2},${height / 2})`)
+          .transition()
+          .duration(duration)
+          .attr(
+            "transform",
+            `translate(${width / 2},${height / 2})scale(0.01)rotate(180)`
+          )
+          .style("opacity", 0)
+          .on("end", () => {
+            svg
+              .attr("transform", null)
+              .style("opacity", 1)
+              .selectAll("*")
+              .remove();
+            resolve();
+          });
+      });
+    }
+
+    function endAll(transition, callback) {
+      var n;
+      if (transition.empty()) {
+        callback();
+      } else {
+        n = transition.size();
+        transition.on("end", function () {
+          n--;
+          if (n === 0) {
+            callback();
+          }
+        });
+      }
+    }
+  }, []);
+
+  return (
+    <div id="app">
+      <svg></svg>
+    </div>
+  );
+};
+
+export default Show;
