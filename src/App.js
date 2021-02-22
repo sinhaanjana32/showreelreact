06bc26fee2f707a02373477a0dc2d0536d4ef9d2
Anjana <sinhaanjana32@gmail.com>
2021-02-22 15:11:57 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Show from "./Show";
+import Typography from "@material-ui/core/Typography";
 
 function App() {
   return (
     <div className="App">
+      <Typography variant="h6" color="secondary" className="typo">
+        ShowReel: Multiple graphs with same data
+      </Typography>
+      <Typography variant="body2" color="secondary" className="typo">
+        D3 Observable code converted to use in React!
+      </Typography>
       <Show />
     </div>
   );
